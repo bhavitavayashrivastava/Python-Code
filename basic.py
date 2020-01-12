c9927ea408b800266d96b3726853dd7c9c90ab41
Bhavitavaya Prakash Shrivastava <42152956+bhavitavayashrivastava@users.noreply.github.com>
2020-01-12 06:30:06 +0530

    Basic_python
    some basic functions

---
+++ basic.py
@@ -0,0 +1,18 @@
+''' This is basic Python program with escape sequences '''
+
+
+# Problem statement Print 
+
+1) \\
+2) /\/\/\/\/\/\/\  these mountain ranges
+3) "this is         awesome place "   (Include spaces using escape sequence)
+4) \" \n \t \'      print this as output
+
+
+print("this is \\\\ double backslash")
+
+print("these are /\\/\\/\\/\\/\\ mountains")
+
+print("he is\t awesome")
+
+print(" \\\" \\n \\t \\\'")
